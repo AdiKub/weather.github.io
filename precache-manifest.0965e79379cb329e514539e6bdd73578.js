fabaf5ac6f3029e40a678fdfd2ed5ab2ccbc2c2f
adilet <adiletkub@gmail.com>
2019-10-04 15:15:45 +0600

    for test

---
+++ precache-manifest.0965e79379cb329e514539e6bdd73578.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c4f6bb8591b38f5d7cba7921a8a6f8",
+    "url": "/AdiKub/weather.github.io/index.html"
+  },
+  {
+    "revision": "727e28d6c448e743ee1b",
+    "url": "/AdiKub/weather.github.io/static/css/main.8d66490a.chunk.css"
+  },
+  {
+    "revision": "08de5b2649d1ac712ffb",
+    "url": "/AdiKub/weather.github.io/static/js/2.8782ad22.chunk.js"
+  },
+  {
+    "revision": "727e28d6c448e743ee1b",
+    "url": "/AdiKub/weather.github.io/static/js/main.d01b0937.chunk.js"
+  },
+  {
+    "revision": "4fbcfb3b5aa655831366",
+    "url": "/AdiKub/weather.github.io/static/js/runtime~main.f0635d3a.js"
+  }
+]);
